7858f35cd770842a08af79f64555f9df734caa5d
Arshad <arshadmahmed0786@gmail.com>
2026-06-13 21:11:55 +0530

    Just Started

---
+++ client/src/components/Landingpage.jsx
@@ -0,0 +1,14 @@
+import react from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Landingpage = () => {
+    return (
+        <div>
+            <Navbar />
+            <Footer />
+        </div>
+    );
+    }
+
+export default Landingpage;
