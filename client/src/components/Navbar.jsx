77f4a404fb533b9452b666b3d42be98d266f6596
Arshad <arshadmahmed0786@gmail.com>
2026-06-14 22:31:33 +0530

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Anime from './Anime'
+import Books from './Books'
+import CreateAccount from './CreateAccount'
+import SignIn from './SignIn'
+import Games from './Games'
+import Movies from './Movies'
+import TvSeries from './TvSeries'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <Anime />
+      <Books />
+      <CreateAccount />
+      <SignIn />
+      <Games />
+      <Movies />
+      <TvSeries />
+    </div>
   )
 }
 
