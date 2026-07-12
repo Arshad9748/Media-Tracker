a45e8413e7441345ca9bc6184c25a12b917c471f
Arshad <arshadmahmed0786@gmail.com>
2026-07-12 22:31:36 +0530

    Auth

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import {useEffect,useState} from 'react'
 
 const SignUp = () => {
+
+const 
+const[signIn,setSignIn] = useState()
   return (
-    <div>SignUp</div>
+    <div>
+      <div>
+        <input type='text' placeholder='Enter your username.'>User Name:</input>
+        <input type='email' placeholder='Enter your email address.'>E-mail:</input>
+        <input type='password' placeholder='Enter your password.'>Password:</input>
+        <input type='password' placeholder='Confirm your password.'>Confirm Password:</input>
+        <button type='submit'>Sign In</button>
+        <butoon type='submit'>Sign Up</butoon> 
+      </div>
+    </div>
   )
 }
 
